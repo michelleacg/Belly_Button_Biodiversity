a575121f3ec797cd9f05dabd1ea88fd279bb6cb5
michelleacg <111101012+michelleacg@users.noreply.github.com>
2023-01-27 23:47:54 -0800

    commit

--- Challenge/charts.js
+++ Challenge/charts.js
@@ -61,16 +61,19 @@ function buildCharts(sample) {
     console.log(data);
 
     // Deliverable 1: 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var metadata = data.metadata
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
-
+    
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
-
+    var results = resultsArrayc[0];
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
 
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids  = results.otu_ids;
+    var otu_labels = results.otu_labels;
+    var sample_values = results.sample_values;
 
     // Deliverable 3: 3. Create a variable that holds the washing frequency.
 
